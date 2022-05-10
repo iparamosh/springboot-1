bb2cef05f79d0fe1f4afb67a147bdc4202e5ba4e
Illia Paramosh <91302209+iparamosh@users.noreply.github.com>
2022-05-10 16:25:16 +0000

    Initial commit

---
+++ src/main/java/com/bothellcs/demo/GreetingController.java
@@ -0,0 +1,26 @@
+package com.bothellcs.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GreetingController {
+
+  @GetMapping("/greeting")
+  public String greetingForm(Model model) {
+    System.out.println("I am the GreetingController GET!!!");
+    model.addAttribute("greeting", new Greeting());
+    return "greeting";
+  }
+
+  @PostMapping("/greeting")
+  public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+    System.out.println("I am the GreetingController POST!!!");
+    model.addAttribute("greeting", greeting);
+    return "result";
+  }
+
+}
